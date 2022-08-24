83b512663121851cb25380e838da3d5963d18b99
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2022-08-23 19:09:18 -0500

    Added greeting, created first employee instance

--- src/hrapp/HRApp.java
+++ src/hrapp/HRApp.java
@@ -14,7 +14,9 @@ public class HRApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Welcome to the HR App!");
+        
+        Employee e1 = new Employee(1, "Peter P.", 4000);
     }
     
 }
