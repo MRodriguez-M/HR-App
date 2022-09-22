b417e6040d67b33b29513d1eb00a85d8010bcd15
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2022-09-22 18:05:35 -0500

    Created instances of Employee and Employee[], testing functionality of addEmployees/getEmployees

--- src/hrapp/HRApp.java
+++ src/hrapp/HRApp.java
@@ -17,7 +17,16 @@ public class HRApp {
         System.out.println("Welcome to the HR App!");
         
         Employee e1 = new Employee(1, "Peter P.", 4000);
+        Employee e2 = new Employee(2, "Otto O.", 10000);
         Department d1 = new Department("Research and Development");
+        
+        Employee[] employees = {e1, e2};
+        d1.addEmployees(employees);
+        
+        System.out.println(d1.toString());
+        for(Employee e: d1.getEmployees()) {
+            System.out.println("Employee: " + e);
+        }
     }
     
 }
